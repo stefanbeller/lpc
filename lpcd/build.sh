fc698602471d7981cd3c54c3e0955d8b2600d605
Jonathan Palecek <jonathan@creativecommons.org>
2012-05-07 16:12:19 -0500

    updated linking script

--- lpcd/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cat src/header.js > lpcd.js
-cat src/map_loader.js > lpcd.js
-cat src/controls.js > lpcd.js
-cat src/actor_model.js > lpcd.js
---
+++ lpcd/link.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cat src/header.js > lpcd.js
+cat src/map_loader.js >> lpcd.js
+cat src/controls.js >> lpcd.js
+cat src/actor_model.js >> lpcd.js
+cat src/graphics.js >> lpcd.js
